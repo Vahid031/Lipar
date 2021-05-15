2c00e0ddea5877aec9ecf33e46b5b6c0ca734f79
Vahid Goodarzi <Vahid031@yahoo.com>
2021-05-15 13:13:11 +0430

    modify project foldering

--- sample/Market.Presentation.Api/Startup.cs
+++ sample/Market.Presentation.Api/Startup.cs
@@ -20,7 +20,6 @@ namespace Market.Presentation.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLiparServices(Configuration, nameof(Lipar), nameof(Market));
@@ -40,7 +39,6 @@ namespace Market.Presentation.Api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
