30471794e8455e33ffd8621d199492c879d2ad41
Vahid Goodarzi <Vahid031@yahoo.com>
2022-01-09 09:57:46 +0330

    change prefix swagger

--- src/Lipar.Presentation.Api/Extensions/ConfigurationExtensions.cs
+++ src/Lipar.Presentation.Api/Extensions/ConfigurationExtensions.cs
@@ -13,7 +13,7 @@ namespace Lipar.Presentation.Api.Extensions
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint(liparOptions.Swagger.Url, liparOptions.Swagger.Name);
-                c.RoutePrefix = "swagger";
+                c.RoutePrefix = "";
             });
 
             app.UseApiExceptionHandler();
