15c8a91df62363994b9e43a16e783f72e97ea4e0
Vahid Goodarzi <Vahid031@yahoo.com>
2022-06-17 23:05:02 +0430

    add mongo project

---
+++ src/Lipar.Infrastructure.Data.Mongo/Commands/BaseCommandDbContext.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using Lipar.Core.Contract.Data;
+
+namespace Lipar.Infrastructure.Data.Mongo.Commands
+{
+    public abstract class BaseCommandDbContext 
+    {
+
+        #region Ctreator and Configuration
+        //public BaseCommandDbContext(DbContextOptions options) : base(options)
+        //{
+
+        //}
+
+        protected BaseCommandDbContext()
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.AddEntityId();
+        //    modelBuilder.AddAuditableProperties();
+
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+        #endregion
+
+        #region Commit Process
+
+        //public async Task<int> SaveChangesAsync()
+        //{
+        //    ChangeTracker.DetectChanges();
+        //    ChangeTracker.AutoDetectChangesEnabled = false;
+
+        //    ChangeTracker.SetShadowProperties();
+        //    AddEntityChangesInterceptors();
+        //    AddOutboxEvetItems();
+
+        //    var rowAffect = await base.SaveChangesAsync();
+
+        //    ChangeTracker.AutoDetectChangesEnabled = true;
+
+        //    return rowAffect;
+        //}
+
+        //private void AddEntityChangesInterceptors()
+        //{
+        //    var entityChangesInterceptors = ChangeTracker.GetEntityChangesInterceptor();
+        //    var repository = this.GetService<IEntityChangesInterceptorRepository>();
+
+        //    repository.AddEntityChanges(entityChangesInterceptors);
+        //}
+
+        //private void AddOutboxEvetItems()
+        //{
+        //    var changedAggregates = ChangeTracker.GetAggregatesWithEvent();
+        //    var repository = this.GetService<IOutBoxEventRepository>();
+
+        //    repository.AddOutboxEvetItems(changedAggregates);
+        //}
+
+        #endregion
+    }
+}
---
+++ src/Lipar.Infrastructure.Data.Mongo/Commands/BaseCommandRepository.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Lipar.Core.Contract.Data;
+using Lipar.Core.Domain.Entities;
+
+namespace Lipar.Infrastructure.Data.Mongo.Commands
+{
+    public class BaseCommandRepository<TEntity, TDbContext> : ICommandRepository<TEntity>, IUnitOfWork
+        where TEntity : AggregateRoot
+        where TDbContext : BaseCommandDbContext
+    {
+        protected IMongoCollection<TEntity> _collection;
+        private IMongoBaseContext _context;
+
+        public BaseCommandRepository(TDbContext db)
+        {
+            this.db = db;
+        }
+
+
+        #region sync Func
+        public void Delete(TEntity entity)
+        {
+            _collection.FindOneAndDelete(e => e.Id == entity.Id);
+        }
+
+        public TEntity Get(EntityId id)
+        {
+            return db.Set<TEntity>().SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            db.Set<TEntity>().Add(entity);
+        }
+
+        public bool Exists(Expression<Func<TEntity, bool>> expression)
+        {
+            return db.Set<TEntity>().Any(expression);
+        }
+
+        public TEntity GetGraph(EntityId id)
+        {
+            var graphPath = db.GetIncludePaths(typeof(TEntity));
+            IQueryable<TEntity> query = db.Set<TEntity>().AsQueryable();
+            var temp = graphPath.ToList();
+            foreach (var item in graphPath)
+            {
+                query = query.Include(item);
+            }
+            return query.SingleOrDefault(c => c.Id == id);
+        }
+
+        public int Commit()
+        {
+            return db.SaveChanges();
+        }
+
+        public bool Exists(EntityId id)
+        {
+            return db.Set<TEntity>().Any(m => m.Id == id);
+        }
+        #endregion
+
+        #region Async Func
+
+        public async Task InsertAsync(TEntity entity)
+        {
+            await db.Set<TEntity>().AddAsync(entity);
+        }
+
+        public async Task<TEntity> GetAsync(EntityId id)
+        {
+            return await db.Set<TEntity>().SingleOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<TEntity> GetGraphAsync(EntityId id)
+        {
+            var graphPath = db.GetIncludePaths(typeof(TEntity));
+            IQueryable<TEntity> query = db.Set<TEntity>().AsQueryable();
+            var temp = graphPath.ToList();
+            foreach (var item in graphPath)
+            {
+                query = query.Include(item);
+            }
+            return await query.SingleOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await db.Set<TEntity>().AnyAsync(expression);
+        }
+
+        public async Task<bool> ExistsAsync(EntityId id)
+        {
+            return await db.Set<TEntity>().AnyAsync(m => m.Id == id);
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await db.SaveChangesAsync();
+        }
+        #endregion
+    }
+}
+
