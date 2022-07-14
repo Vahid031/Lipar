293f091be8b108d2bcd5209e90d0289ed7c963f8
Vahid Goodarzi <Vahid031@yahoo.com>
2022-07-14 11:51:42 +0430

    change folder

--- sample/Market.Presentation.Api/Program.cs
+++ sample/Market.Presentation.Api/Program.cs
@@ -3,61 +3,26 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Microsoft.Extensions.Configuration;
-using System.Threading.Tasks;
 using Market.Infrastructure.Data.Identity.Seeds;
 using Microsoft.AspNetCore.Identity;
 using Market.Infrastructure.Data.Identity.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Market.Presentation.Api;
 
-namespace Market.Presentation.Api;
 
-public class Program
-{
-    public static async Task Main(string[] args)
-    {
-        //Read Configuration from appSettings
-        var config = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.Serilog.json")
-        .Build();
-        //Initialize Logger
-        Log.Logger = new LoggerConfiguration()
+
+
+//Read Configuration from appSettings
+var config = new ConfigurationBuilder()
+.AddJsonFile("appsettings.Serilog.json")
+.Build();
+//Initialize Logger
+Log.Logger = new LoggerConfiguration()
         .ReadFrom.Configuration(config)
         .CreateLogger();
-        
-        
-        var host = CreateHostBuilder(args).Build();
-        
-        using (var scope = host.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            
-            try
-            {
-                var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-                
-                await DefaultRoles.SeedAsync(userManager, roleManager);
-                await DefaultSuperAdmin.SeedAsync(userManager, roleManager);
-                
-                Log.Information("Finished Seeding Default Data");
-                
-                host.Run();
-                Log.Information("Application Starting");
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-        
-    }
-    
-    public static IHostBuilder CreateHostBuilder(string[] args)
-    => Host.CreateDefaultBuilder(args)
+
+
+var app = Host.CreateDefaultBuilder(args)
     .ConfigureWebHostDefaults(webBuilder =>
     {
         webBuilder.UseStartup<Startup>()
@@ -67,9 +32,33 @@ public class Program
             config.AddJsonFile("appsettings.json", optional: true)
             .AddJsonFile("appsettings.Serilog.json", optional: true)
             .AddJsonFile("appsettings.Lipar.json", optional: true);
-        })
-        .UseSerilog();
-    });
-}
+        });
+    }).Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        await DefaultRoles.SeedAsync(userManager, roleManager);
+        await DefaultSuperAdmin.SeedAsync(userManager, roleManager);
+
+        Log.Information("Finished Seeding Default Data");
+
+        app.Run();
+        Log.Information("Application Starting");
+    }
+    catch (Exception ex)
+    {
+        Log.Fatal(ex, "Application start-up failed");
+    }
+    finally
+    {
+        Log.CloseAndFlush();
+    }
+}
 
