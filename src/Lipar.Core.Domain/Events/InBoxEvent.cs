03d03199a8abadf84a2a8f8df68f704945535f30
Vahid Goodarzi <Vahid031@yahoo.com>
2023-06-09 18:19:15 +0330

    ada statuscode

--- src/Lipar.Core.Domain/Events/InBoxEvent.cs
+++ src/Lipar.Core.Domain/Events/InBoxEvent.cs
@@ -5,9 +5,21 @@ namespace Lipar.Core.Domain.Events;
 public class InBoxEvent
 {
     public Guid Id { get; set; }
-    public string MessageId { get; set; }
     public string OwnerService { get; set; }
-    public DateTime ReceivedDate { get; set; }
+    public string Paload { get; set; }
+    public InBoxEventStatus Status { get; set; }
+    public int RetryCount { get; set; }
+    public DateTime ReceivedAt { get; set; }
+}
+
+public enum InBoxEventStatus
+{
+    Unknown = 0,
+    Received = 1,
+    Scheduled = 2,
+    InProcess = 3,
+    Succeded = 4,
+    Failed = 5,
 }
 
 
